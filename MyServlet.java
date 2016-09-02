097a8534091577c945d7b36a1d2ff7b48b46c4b1
NilaySTAR <nilay.das@hotmail.com>
2016-09-03 01:08:01 +0600

    Add files via upload

---
+++ MyServlet.java
@@ -0,0 +1,38 @@
+package comm.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/controller.do")
+public class MyServlet extends HttpServlet {
+	
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		processRequest(request, response);
+	
+	}
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			response.setContentType("text/html");
+			PrintWriter out=response.getWriter();
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
